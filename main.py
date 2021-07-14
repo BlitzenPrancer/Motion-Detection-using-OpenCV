b6b58660149f974022b58711eeb5a8871fae2859
Blitzen Prancer <76688640+BlitzenPrancer@users.noreply.github.com>
2021-07-14 23:33:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0) # 0 denotes the default webcam
 while cam.isOpened():
     ret, frame = cam.read()
     if cv2.waitKey(10) == ord('q'):

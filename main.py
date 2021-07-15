b64f2227a3ca44f67d021ea58424419f83164b74
Blitzen Prancer <76688640+BlitzenPrancer@users.noreply.github.com>
2021-07-15 22:44:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import cv2
 cam = cv2.VideoCapture(0) # 0 denotes the default webcam
 while cam.isOpened():
     ret, frame = cam.read()
-    if cv2.waitKey(10) == ord('q'):
+    if cv2.waitKey(10) == ord('q'): # this makes the computer wait for 10 seconds until q key is pressed
         break
     cv2.imshow('cam', frame)

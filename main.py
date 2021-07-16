fced0d0ab06f019747e251ffd6b4e05878b7b394
BlitzenPrancer <blitzenprancer666@gmail.com>
2021-07-16 23:10:18 +0530

    commiting

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import cv2
-cam = cv2.VideoCapture(0) # 0 denotes the default webcam
+cam = cv2.VideoCapture(0)
 while cam.isOpened():
-    ret, frame = cam.read()
-    if cv2.waitKey(10) == ord('q'): # this makes the computer wait for 10 seconds until q key is pressed
+    ret, frame1 = cam.read()
+    ret, frame2 = cam.read()
+    # creating a difference between frame1 and frame2
+    diff = cv2.absdiff(frame1, frame2)
+    # converting difference to grey colour
+    gray = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
+    # converting grey to blur
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    if cv2.waitKey(10) == ord('q'):
         break
-    cv2.imshow('cam', frame)
+    cv2.imshow('cam', blur)

7e80dd70825a655dd46f0349f3ba685de3ecdd6a
BlitzenPrancer <blitzenprancer666@gmail.com>
2021-07-17 11:41:48 +0530

    added audio

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import winsound
 cam = cv2.VideoCapture(0)
 while cam.isOpened():
     ret, frame1 = cam.read()
@@ -16,7 +17,14 @@ while cam.isOpened():
     # finding contours
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # drawing contours
-    cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2 )
+    # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2 )
+    for c in contours:
+        if cv2.contourArea(c) < 5000:
+            continue
+        # getting x, y, width,and height of contours greater than 5000
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(frame1, (x,y), (x + w, y + h), (0, 255, 0), 2)
+        winsound.PlaySound('audio.wav', winsound.SND_ASYNC)
     if cv2.waitKey(10) == ord('q'):
         break
     cv2.imshow('cam', frame1)

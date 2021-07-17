936508503cae5f96432c318260baa15fe51ede25
BlitzenPrancer <blitzenprancer666@gmail.com>
2021-07-17 11:02:08 +0530

    added contours

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ while cam.isOpened():
     gray = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
     # converting grey to blur
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # creating threshold to get rid of the noise
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    # making things bigger with dilation
+    dilated = cv2.dilate(thresh, None, iterations = 3)
+    # finding contours
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # drawing contours
+    cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2 )
     if cv2.waitKey(10) == ord('q'):
         break
-    cv2.imshow('cam', blur)
+    cv2.imshow('cam', frame1)
